de8ce654ccde465c8824473655a3cd5e99d6d761
atenshy <70568627+a10shy@users.noreply.github.com>
2025-10-06 02:54:14 +0530

    Update vite.config.ts
    added allowed hosts

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,9 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    allowedHosts: [
+      "spark-chiang-folio-47267-90175-44732-85.onrender.com" // ✅ Add your Render domain here
+    ],
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
